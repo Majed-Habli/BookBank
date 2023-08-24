4eb16eb9ec44fd0e178b27b569d31517adadb37d
User <majedhabli2020@gmail.com>
2023-08-24 07:15:03 +0300

    Server <> Updated create post function.

--- Server/controllers/posts.controllers.js
+++ Server/controllers/posts.controllers.js
@@ -1,23 +1,35 @@
 const User = require("../models/users.model")
+
 const createPost = async (req, res)=>{
+
+try {
+
     console.log("createPost")
     const {name, author, review, genere} = req.body;
 
-    const user = User.findById(req.user._id);
+    const user = await User.findById(req.user._id);
     
     const newPost = {name, author, review, genere, count_like: 0}
-    const updatedUser = User.updateOne({
+    // res.send(newPost)
+    const updatedUser = await User.updateOne({
         email: req.user.email,
     }, {
         $push: {
             posts: newPost
         }
-    })
+    }).exec();
 
     console.log(updatedUser)
     
 
-    res.send("Verfied")
+    res.send(updatedUser);
+
+}catch (error){
+
+        console.error(error);
+        res.status(500).send("An error occurred");
+        
+    }
 }
 
 
