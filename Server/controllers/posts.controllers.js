66dc96cc37cbcd3e9562aba3991866735be2f38a
User <majedhabli2020@gmail.com>
2023-08-25 07:44:23 +0300

    Server <> Finished creating the apis.

--- Server/controllers/posts.controllers.js
+++ Server/controllers/posts.controllers.js
@@ -1,112 +1,134 @@
-const User = require("../models/users.model")
+const User = require("../models/users.model");
 
-const createPost = async (req, res)=>{
-
-try {
-
-    console.log("createPost")
-    const {name, author, review, genere} = req.body;
+const createPost = async (req, res) => {
+  try {
+    console.log("createPost");
+    const { name, author, review, genere } = req.body;
 
     const user = await User.findById(req.user._id);
-    
-    const newPost = {name, author, review, genere, count_like: 0}
-    
-    const updatedUser = await User.updateOne({
-        email: req.user.email,
-    }, {
-        $push: {
-            posts: newPost
-        }
-    }).exec();
 
-    console.log(updatedUser)
-    
+    const newPost = { name, author, review, genere, count_like: 0 };
+
+    const updatedUser = await User.updateOne(
+      {
+        email: req.user.email,
+      },
+      {
+        $push: {
+          posts: newPost,
+        },
+      }
+    ).exec();
+
+    console.log(updatedUser);
 
     res.send(updatedUser);
-
-}catch (error){
-
-        console.error(error);
-        res.status(500).send("An error occurred");
-
-    }
-}
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred");
+  }
+};
 
 const getFollowingPosts = async (req, res) => {
+  const user = await User.findById(req.user._id);
+  console.log("user", user);
 
-    const user = await User.findById(req.user._id)
-    console.log("user", user)
+  const user_following = user.following;
+  console.log("im following", user_following);
 
-    const user_following = user.following
-    console.log("im following", user_following)
+  const following = await User.find({ _id: { $in: user_following } });
+  console.log("the people data i follow", following);
 
-    const following = await User.find({_id : { $in: user_following }} )
-    console.log("the people data i follow", following)
+  const postsFromFollowedUsers = [];
 
-    const postsFromFollowedUsers = [];
+  for (const followingUser of following) {
+    console.log("inner for loop ", followingUser);
+    postsFromFollowedUsers.push(...followingUser.posts);
+  }
 
-        for (const followingUser of following) {
-            console.log("inner for loop ", followingUser)
-            postsFromFollowedUsers.push(...followingUser.posts);
-        }
+  const search = req.body.search;
 
-        const search = req.body.search
-        
-        if(!search === ''){
-            
-            const posts = await postsFromFollowedUsers.find({ $or: [{author : {$regex :'^search'}}, {genere : {$regex : '^search'}}, {name : {$regex : '^search'}}]})
-            res.send(posts);
+  if (!search === "") {
+    const posts = await postsFromFollowedUsers.find({
+      $or: [
+        { author: { $regex: "^search" } },
+        { genere: { $regex: "^search" } },
+        { name: { $regex: "^search" } },
+      ],
+    });
+    res.send(posts);
+  } else {
+    res.send(postsFromFollowedUsers);
+  }
 
-        }else{
-            res.send(postsFromFollowedUsers)
-        }
-
-        res.send(posts);
-}
-
+  res.send(posts);
+};
+//gave up on this one
 const likePost = async (req, res) => {
+  // const user = await User.findById(req.user._id);
 
-    // const user = await User.findById(req.user._id)
-    // console.log("user", user)
+  // const usersFollowing = user.following;
 
-    // const user_following = user.following
-    // console.log("im following", user_following)
+  // const userFollowingObjects = await User.find({
+  //   _id: { $in: usersFollowing },
+  // });
 
-    // const following = await User.find({_id : { $in: user_following }} )
-    // console.log("the people data i follow", following)
+  // const userWithBook = userFollowingObjects.find((userObject) => {
+  //   const posts = userObject.posts;
+  //   return posts.find((post) => post.id === req.body.id);
+  // });
 
-    // const postsFromFollowedUsers = [];
+  // if (!userWithBook) {
+  //   res.status(404).send({ message: "Not found" });
+  // }
 
-    //     for (const followingUser of following) {
+  // const postToBeUpdated = userWithBook.posts.find(
+  //   (post) => post.id === req.body.id
+  // );
 
-    //         console.log("inner for loop ", followingUser)
-    //         postsFromFollowedUsers.push(...followingUser.posts);
+  // const alreadyLikedPost = postToBeUpdated.likedUsers.find(
+  //   (user) => user.id === req.user._id
+  // );
 
-    //     }
+  // // put the id
+  // const updatedLikedUsers = alreadyLikedPost
+  //   ? postToBeUpdated.likedUsers.filter((user) => user !== req.user._id)
+  //   : [...postToBeUpdated.likedUsers, {}];
 
-    //     const liked = req.body.liked
+  // // final step is update
+  // res.send("kkkk");
+  // return;
 
-    //     if(!liked === 0){
+  res.send("hi");
+  return;
 
-    //         liked ===0
-    //         const posts = await postsFromFollowedUsers.update({ $set: {count_like : liked}})
-    //         // const posts = postsFromFollowedUsers.map(item => item)
+  // const following = await User.find({ _id: { $in: user_following } });
+  // console.log("the people data i follow", following);
 
-    //         res.send(posts);
+  // const postsFromFollowedUsers = [];
 
-    //     }else{
+  // for (const followingUser of following) {
+  //   console.log("inner for loop ", followingUser);
+  //   postsFromFollowedUsers.push(...followingUser.posts);
+  // }
 
-    //         liked === 1
-    //         res.send(posts);
-    //         for (const post of postsFromFollowedUsers){
-    //             const posts = await post.updateOne({ $set: {count_like : liked}})
-                
+  // const liked = req.body.liked;
 
-    //         }
+  // if (!liked === 0) {
+  //   liked === 0;
+  //   const posts = await postsFromFollowedUsers.update({
+  //     $set: { count_like: liked },
+  //   });
+  //   // const posts = postsFromFollowedUsers.map(item => item)
 
-    //     }
-}
+  //   res.send(posts);
+  // } else {
+  //   liked === 1;
+  //   res.send(posts);
+  //   for (const post of postsFromFollowedUsers) {
+  //     const posts = await post.updateOne({ $set: { count_like: liked } });
+  //   }
+  // }
+};
 
-
-
-module.exports = {createPost, getFollowingPosts, likePost}
+module.exports = { createPost, getFollowingPosts, likePost };
--- Server/models/users.model.js
+++ Server/models/users.model.js
@@ -1,30 +1,35 @@
 const mongoose = require("mongoose");
 
 const bookSchema = new mongoose.Schema({
-    name: String,
-    author: String,
-    genere: String,
-    review: String,
-    // image: String,
-    count_like: Number
-})
+  name: String,
+  author: String,
+  genere: String,
+  review: String,
+  // image: String,
+  likedUsers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    email: {
-        type: String,
-        require: "Custom: Email is required"
+  name: String,
+  email: {
+    type: String,
+    require: "Custom: Email is required",
+  },
+  password: String,
+  posts: [bookSchema],
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
-    password: String,
-    posts:[bookSchema],
-    following: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-        }
-    ],
-})
+  ],
+});
 
-const model = mongoose.model("User", userSchema)
-const modelBook = mongoose.model("Book", bookSchema)
-module.exports = model, modelBook;
+const model = mongoose.model("User", userSchema);
+const modelBook = mongoose.model("Book", bookSchema);
+(module.exports = model), modelBook;
